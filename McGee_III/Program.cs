95a9c81b66e4713692823232bf4a49ada0b3e34d
Eric Thompson <Skjeden@Erics-MacBook-Air.local>
2015-11-26 00:41:55 -0500

    Minor updates

--- McGee_III/Program.cs
+++ McGee_III/Program.cs
@@ -6,24 +6,41 @@ namespace McGee_III
 	{
 		public static void Main (string[] args)
 		{
-			string input = "";
-
 			Console.WriteLine ("+ ----------------------------------- +");
 			Console.WriteLine ("|   The Adventure of Mrs. McGee III   |");
 			Console.WriteLine ("+ ----------------------------------- +");
 			Console.WriteLine ("            ~ Press Enter ~            ");
 			Console.ReadLine ();
-
 			Console.Clear ();
+
 			Console.Write ("Please enter your name, adventurer.\n\n> ");
 			Character player = new Character (Console.ReadLine ());
-
 			Console.WriteLine ("\nWelcome, Mrs. {0} McGee!", player.Name);
+			Console.WriteLine ("\nI understand you may be a new player.");
+			Console.WriteLine ("\nWould you like a tutorial? (y/n)"); ynTrap ();
 
-			while (input != "quit") {
-				Console.Write ("\n> ");
-				input = Console.ReadLine ().ToLower ();
-			}
+
+		
+//				
+//			if (input == "y") {
+//				// Run help command
+//			} else if (input == "n") {
+//				Console.WriteLine ("Very well, enjoy your adventure, {0}!", player.Name);
+//			} else {
+//				
+		
+
+
+
+//			while (input != "quit") {
+//				Console.Write ("\n> ");
+//				input = Console.ReadLine ().ToLower ();
+//			}
+		}
+
+		public static void ynTrap ()
+		{
+			
 		}
 	}
 }
