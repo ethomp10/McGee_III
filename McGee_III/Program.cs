ee328ec62849d3647e8103bfc3c477e94748d5a6
Eric Thompson <eric.r.thompson@icloud.com>
2015-11-26 02:32:42 -0500

    Added basic functions
    Player can now type commands such as "help" or "quit".

--- McGee_III/Program.cs
+++ McGee_III/Program.cs
@@ -6,6 +6,7 @@ namespace McGee_III
     {
         public static void Main(string[] args)
         {
+            Console.Title = "The Adventure of Mrs. McGee III";
             string input;
 
             Console.WriteLine("+ ----------------------------------- +");
@@ -16,27 +17,51 @@ namespace McGee_III
             Console.Clear();
 
             Console.Write("Please enter your name, adventurer.\n\n> ");
-            Character player = new Character(Console.ReadLine());
+            Character player = new Character(Console.ReadLine()); // Get players name
             Console.WriteLine("\nWelcome, Mrs. {0} McGee!", player.Name);
             Console.WriteLine("I understand you may be a new player.");
             Console.WriteLine("Would you like a tutorial? (y/n)"); input = ynTrap();
 
             if (input == "y")
             {
-                // Run help command
+                showHelp();
             }
             else if (input == "n")
             {
                 Console.WriteLine("\nVery well, enjoy your adventure, {0}!", player.Name);
             }
 
-                while (input != "quit")
+            while (input != "quit")
+            {
+                Console.Write("\n> ");
+                input = Console.ReadLine().ToLower();
+                switch (input)
                 {
-                    Console.Write("\n> ");
-                    input = Console.ReadLine().ToLower();
+                    case "help":
+                        showHelp();
+                        break;
+                    case "cmd":
+                        printCommands();
+                        break;
+                    default:
+                        Console.WriteLine("\nI don't understand this command.");
+                        Console.WriteLine("Type \"cmd\" for a list of usable commands.");
+                        break;
                 }
             }
+        }
 
+        // Print user manual
+        public static void showHelp()
+        {
+            Console.Clear();
+            Console.WriteLine("~ The Adventure of Mrs. McGee III: Official User Manual ~");
+            Console.WriteLine("\nThroughout your adventure, you will have to control yourself by typing commands.");
+            Console.WriteLine("Whenever an angle bracket \">\" is printed, you may enter any of the following commands:");
+            printCommands();
+        }
+
+        // Validate (y/n) input
         public static string ynTrap()
         {
             bool valid = false;
@@ -45,7 +70,7 @@ namespace McGee_III
             while (valid == false)
             {
                 Console.Write("\n> ");
-                input = Console.ReadLine();
+                input = Console.ReadLine().ToLower();
                 switch (input)
                 {
                     case "y":
@@ -59,6 +84,15 @@ namespace McGee_III
             }
 
             return input;
+        }
+
+        // Print list of usable commands
+        public static void printCommands()
+        {
+            Console.WriteLine("\nCOMMAND\t\tDESCRIPTION");
+            Console.WriteLine("help\t\tShows manual");
+            Console.WriteLine("cmd\t\tLists these commands");
+            Console.WriteLine("quit\t\tExits the application");
         }
     }
 }
